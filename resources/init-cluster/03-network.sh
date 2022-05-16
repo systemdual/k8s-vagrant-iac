0641c504c92d2db6a4fe7195dc84496866189809
Mauro Fernández Quiñones <maurofq1991@gmail.com>
2022-05-16 12:45:17 +0200

    Update 03-network.sh

--- resources/init-cluster/03-network.sh
+++ resources/init-cluster/03-network.sh
@@ -1,2 +1,3 @@
 # network.sh
-kubectl apply -f "https://cloud.weave.works/k8s/net?k8s-version=$(sudo kubectl version | base64 | tr -d '\n')"
+#kubectl apply -f "https://cloud.weave.works/k8s/net?k8s-version=$(sudo kubectl version | base64 | tr -d '\n')"
+kubectl create -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml
